6b773a67642e9f4273a3dde0addeb12b5241f660
yuri <yuri_bs@hotmail.com>
2025-11-20 17:54:25 -0300

    atualizações pntuais e melhorias.

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware para analisar o corpo das requisições JSON e URL-encoded
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Configura o CORS
 app.use(cors({
